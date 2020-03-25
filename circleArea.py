c6239ec4d711e3a60ec8d91ea2f432ae7e6977f5
IdentitySeal <teemealeheen@gmail.com>
2020-03-25 21:30:19 +0100

    Create circleArea.py

---
+++ circleArea.py
@@ -0,0 +1,11 @@
+def circle_area():
+
+    print("Please,input the radius of circle required ")
+
+    radius = int(input("Radius: ))
+
+    area = (22 * (radius**2))/7)
+
+    print(area)
+
+calc_circle_area()
